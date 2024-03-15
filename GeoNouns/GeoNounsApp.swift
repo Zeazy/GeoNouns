c9b79b0ae05a867a6bff5ab0b9b9c4c89242ef49
Hamza <alfadelhamza@gmail.com>
2024-03-15 19:17:49 +0000

    Initial Commit

---
+++ GeoNouns/GeoNounsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoNounsApp.swift
+//  GeoNouns
+//
+//  Created by Hamza Al Fadel on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeoNounsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
