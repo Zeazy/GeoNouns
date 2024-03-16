a95148a5a1f7a64eef0ab59df6dc36473e10fa62
Hamza <alfadelhamza@gmail.com>
2024-03-16 11:38:44 +0000

    new files

---
+++ GeoNouns/Views/WalletConnectView.swift
@@ -0,0 +1,18 @@
+//
+//  WalletConnectView.swift
+//  GeoNouns
+//
+//  Created by Hamza Al Fadel on 16/03/2024.
+//
+
+import SwiftUI
+
+struct WalletConnectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WalletConnectView()
+}
