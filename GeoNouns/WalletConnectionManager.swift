4a2b95532e60edf6c6561f2b0ce8c139f77c79a0
Hamza <alfadelhamza@gmail.com>
2024-03-16 10:24:11 +0000

    add walletconnect manager

---
+++ GeoNouns/WalletConnectionManager.swift
@@ -0,0 +1,18 @@
+//
+//  WalletConnectionManager.swift
+//  GeoNouns
+//
+//  Created by Hamza Al Fadel on 16/03/2024.
+//
+
+import Foundation
+import metamask_ios_sdk
+
+class WalletConnectionManager: ObservableObject {
+    @Published var walletConnected = ""
+    
+    func setup() {
+    }
+}
+
+
