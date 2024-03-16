c8ef7a7c538672282e4b100dc35c161d70bd5f6a
Hamza <alfadelhamza@gmail.com>
2024-03-16 11:41:37 +0000

    refactor wallet connec view

--- GeoNouns/Views/WalletConnectView.swift
+++ GeoNouns/Views/WalletConnectView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct WalletConnectView: View {
+    @ObservedObject private var connectionManager = WalletConnectionManager()
+    @State var isConnectPresented = false
+    @State var connectionError: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Button(action: {
+                    Task {
+                        let result = await connectionManager.connectToMetamask()
+                        switch result {
+                        case .success:
+                            print("MetaMask connection succeeded")
+                            isConnectPresented = true
+                        case let .failure(error):
+                            print("Error connecting to MetaMask: \(error.localizedDescription)")
+                            connectionError = error.localizedDescription
+                        }
+                    }
+                }) {
+                    Image("metamask_logo")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+                .padding()
+                
+                if let error = connectionError {
+                    Text("Connection Error: \(error)")
+                        .foregroundColor(.red)
+                        .padding()
+                }
+            }
+            .navigationDestination(isPresented: $isConnectPresented) {
+                // TODO: Replace
+                Text("Connected to MetaMask")
+            }
+        }
     }
 }
 
